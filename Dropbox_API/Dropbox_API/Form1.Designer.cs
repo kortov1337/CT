2aa6e7c9635f4a592cb0bc48615ce4d7c6c8af63
Roman Belkevich <belkevich1337@gmail.com>
2017-02-16 23:07:20 +0300

    inirial commit

---
+++ Dropbox_API/Dropbox_API/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Dropbox_API
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("root");
+            this.detailInfoTab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.detailInfoTB = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.spaceInfo = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.detailInfoTab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detailInfoTab
+            // 
+            this.detailInfoTab.Controls.Add(this.tabPage1);
+            this.detailInfoTab.Controls.Add(this.tabPage2);
+            this.detailInfoTab.Controls.Add(this.tabPage3);
+            this.detailInfoTab.Controls.Add(this.tabPage4);
+            this.detailInfoTab.Controls.Add(this.tabPage5);
+            this.detailInfoTab.Location = new System.Drawing.Point(-4, -1);
+            this.detailInfoTab.Name = "detailInfoTab";
+            this.detailInfoTab.SelectedIndex = 0;
+            this.detailInfoTab.Size = new System.Drawing.Size(790, 465);
+            this.detailInfoTab.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.detailInfoTB);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(782, 439);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Detailed information";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(703, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.tabControl1_Click);
+            // 
+            // detailInfoTB
+            // 
+            this.detailInfoTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.detailInfoTB.Location = new System.Drawing.Point(3, 0);
+            this.detailInfoTB.Name = "detailInfoTB";
+            this.detailInfoTB.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.detailInfoTB.Size = new System.Drawing.Size(696, 440);
+            this.detailInfoTB.TabIndex = 0;
+            this.detailInfoTB.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.spaceInfo);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(782, 439);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Space in storage";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // spaceInfo
+            // 
+            this.spaceInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.spaceInfo.Location = new System.Drawing.Point(0, 1);
+            this.spaceInfo.Name = "spaceInfo";
+            this.spaceInfo.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.spaceInfo.Size = new System.Drawing.Size(696, 440);
+            this.spaceInfo.TabIndex = 1;
+            this.spaceInfo.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(703, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.treeView1);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(782, 439);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Folders";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(782, 439);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(782, 439);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(703, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.treeView1.FullRowSelect = true;
+            this.treeView1.Location = new System.Drawing.Point(13, 7);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "root";
+            treeNode1.Text = "root";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(684, 422);
+            this.treeView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.detailInfoTab);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.tabControl1_Click);
+            this.detailInfoTab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl detailInfoTab;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.RichTextBox detailInfoTB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox spaceInfo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Dropbox_API/Dropbox_API/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dropbox.Api;
+using Dropbox.Api.Users;
+using Dropbox.Api.Files;
+namespace Dropbox_API
+{
+    public partial class Form1 : Form
+    {      
+        static string token = "X0maJoW9_aAAAAAAAAAACZbATZubjd0vm52xwTvrROQEl-6Pc0yl-K-Gm9Bk5UYi";
+        static DropboxClient client = new DropboxClient(token);
+
+        public string[] getNodeName(string path,int pos)
+        {
+            string [] res=new string[2];
+            //path = path.Remove(0, 1);
+            int spos = path.LastIndexOf("/");
+            //if (spos >= 0)
+         //   {
+                res[1] = path.Substring(spos+1, path.Length-spos-1);
+           // }
+            path = path.Remove(spos , path.Length - spos);
+            spos = path.LastIndexOf("/")+1;
+            res[0] = path.Substring(spos , path.Length-spos);
+            return res;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+          FullAccount info = await client.Users.GetCurrentAccountAsync();
+            detailInfoTB.Text = info.Name.DisplayName +Environment.NewLine+ info.Email;
+        }
+
+        private async void tabControl1_Click(object sender, EventArgs e)
+        {
+            FullAccount info = await client.Users.GetCurrentAccountAsync();
+            detailInfoTB.Text = "Display name: " + info.Name.DisplayName + Environment.NewLine + "Surname name: " + 
+            info.Name.Surname + Environment.NewLine + "Email: " + info.Email + Environment.NewLine +"Account type: " + 
+            (info.AccountType.IsBasic?"Basic":info.AccountType.IsBusiness?"Business":info.AccountType.IsPro?"Pro":"Unknown")
+            +Environment.NewLine + "Country: " + info.Country + Environment.NewLine + "Referal link: " + info.ReferralLink;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            tabControl1_Click(sender, e);
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            SpaceUsage space = await client.Users.GetSpaceUsageAsync();
+            spaceInfo.Text = "Used space: " + (space.Used/1000000).ToString() + "MB" + Environment.NewLine + "Free space: "+
+            (space.Allocation.AsIndividual.Value.Allocated- space.Used)/1000000 + "MB" + Environment.NewLine + "Allocated space: " +
+            (space.Allocation.AsIndividual.Value.Allocated/1000000).ToString() + "MB";
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+             ListFolderResult list = await client.Files.ListFolderAsync(new ListFolderArg(string.Empty, true));
+            string tmp="Folder1";
+            TreeNode [] root = treeView1.Nodes.Find("root", false);
+            foreach (var item in list.Entries.Where(i => i.IsFolder))
+            {
+                int pos = item.PathDisplay.LastIndexOf("/");
+                if(pos==0)
+                {
+                    treeView1.BeginUpdate();
+                    root[0].Nodes.Add(item.Name, item.Name);
+                    treeView1.EndUpdate();
+                }
+                else
+                {
+                    string [] names = getNodeName(item.PathDisplay, pos);
+                    TreeNode[] node = root[0].Nodes.Find(names[0], true);
+                    treeView1.BeginUpdate();
+                    node[0].Nodes.Add(names[1], names[1]);
+                    treeView1.EndUpdate();
+                }
+            }
+
+            foreach (var item in list.Entries.Where(i => i.IsFile))
+            {
+                int pos = item.PathDisplay.LastIndexOf("/");
+                if (pos == 0)
+                {
+                    treeView1.BeginUpdate();
+                    root[0].Nodes.Add(item.Name, item.Name);
+                    treeView1.EndUpdate();
+                }
+                else
+                {
+                    string[] names = getNodeName(item.PathDisplay, pos);
+                    TreeNode[] node = root[0].Nodes.Find(names[0], true);
+                    treeView1.BeginUpdate();
+                    node[0].Nodes.Add(names[1], names[1]);
+                    treeView1.EndUpdate();
+                }
+                // TreeNode [] q = root[0].Nodes.Find("Folder1", false);
+                //treeView1.Nodes.Find(tmp,false);
+                //   new TreeNode("sad");
+                // q.n
+                // int w = 0;
+                // Console.WriteLine("F{0,8} {1}", item.AsFile.Size, item.Name);
+            }
+        }
+    }
+}
